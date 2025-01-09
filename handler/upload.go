b282b9f9c33406588e09f6c35b9e6d78ecfe506a
kobidsim <simkhadakobid@gmail.com>
2025-01-10 00:19:16 +0545

    feat: basic file upload functionality created

--- handler/upload.go
+++ handler/upload.go
@@ -1,7 +1,26 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		http.Error(w, "error uploading file", http.StatusInternalServerError)
+	}
+	if r.MultipartForm == nil {
+		http.Error(w, "no files uploaded", http.StatusBadRequest)
+		return
+	}
+	files := r.MultipartForm.File
+	if len(files) == 0 {
+		http.Error(w, "no files uploaded", http.StatusBadRequest)
+	}
 
+	for _, file := range files["files"] {
+		if file != nil {
+			fmt.Fprintf(w, "filename: %v", file.Filename)
+		}
+	}
 }
